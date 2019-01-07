3f20ab344bfe7697b4ef18de076be43ae20ba34a
heshunbin <707952088@qq.com>
2019-01-07 21:56:45 +0800

    login页初版 h

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,17 +1,40 @@
 <template>
-    <div class="page">
-        登录页
-    </div>
+  <div class="page">
+    <mt-header title="短信登录" fixed>
+      <router-link to="/" slot="left">
+        <mt-button icon="back"></mt-button>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+    <mt-field placeholder="请输入手机号" type="tel" v-model="phone" class="phone"></mt-field>
+    <mt-field  placeholder="请输入短信验证码" type="tel" v-model="captcha" class="captcha">
+       <p>获取手机验证码</p>
+    </mt-field>
+  </div>
 </template>
 <script>
 export default {
-    data(){
-        return {
-            
-        }
-    },
-    methods:{
-        
-    }
-}
+  data() {
+    return {};
+  },
+  methods: {}
+};
 </script>
+<style>
+.phone {
+  margin-top: 80px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+/* .captcha {
+  padding-right: none;
+  margin-top: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+.captcha span {
+  color: #fff;
+  background-color: red;
+} */
+</style>
